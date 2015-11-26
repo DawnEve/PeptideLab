bdedb7e7d0d12ab656f998f881dcbc5f698ae897
Jimmy <JimmyMall@live.com>
2015-11-27 06:56:16 +0800

    (1.3)function to file ;

--- wjl/Vgph/getVgene.php
+++ wjl/Vgph/getVgene.php
@@ -1,115 +1,54 @@
 <?php
-
+//----------------------------------
 //定义库的路径，以/结尾
 $path="F:/xampp/htdocs/wjl/Vgph/myLib/";
 //包含库文件
 require $path.'myLibGate.php';
+//----------------------------------
 
-
-//===========================================
-//=====工作代码
-//===========================================
-//获取class值 http://blog.sina.com.cn/s/blog_aadf4b9201011d4t.html
+//获取class值
 if(empty($_GET["c"]))die('invalid URL');//禁止直接访问
 $vclass=$_GET["c"];
 
-//设置变量保存响应值：
-$response= "<div class=separator>Searching parameters</div>";
-$response.= "Done at ".date("Y-m-d H:i:s",time()).", ";
-$response.="in the class: $vclass, ";
-$responseT="";//保存响应的表格
-
-//引进引物;
-//get the q parameter from URL
+//获取引物值
 if(empty($_GET["q"])){
-	$primer0= "CAG GTG CAG CTG CAG GAG TCS G"; 
-	//设置默认引物，====================================bug
-	//不能设置默认引物？？？
+	//$primer0= "CAG GTG CAG CTG CAG GAG TCS G"; //原始引物序列
+	die('please input a primer.');
 }else{
 	$primer0=trim($_GET["q"]);//去掉2端空格
 }
 
 
-
-
-//$primer0= "CAG GTA CAG CTG CAG CAG TCA"; //原始引物序列
+//引物序列确定
 $primer1= preg_replace("/\s/","",$primer0); //删除空格后
 $len=strlen($primer1);//删除空格后的引物长度
-$primer2=replaceATGC($primer1);//替换引物中的简并碱基为ATGC
+$primer=replaceATGC($primer1);//替换引物中的简并碱基为ATGC
 
-//引物序列确定
-$primer=$primer2;
+
+//设置变量保存响应值：
+$response = "<div class=separator>Searching parameters</div>";
+$response .= "Done at ".date("Y-m-d H:i:s",time()).", ";
+$response .= "in the class: $vclass, ";
 
 //输出引物
 $response .= "<b>primer info: </b><div id=primr>
-OriInput:$primer0<br />delSpace:$primer1 length:$len<br />aftSubst:$primer2</div>";
+OriInput:$primer0<br />delSpace:$primer1 length:$len<br />aftSubst:$primer</div>";
 
 
-
-//	sql语句
-	//$sql='insert into user3(name,sex) values("大海", "boy");';
-//	执行sql语句
-	//$a=mysql_query($sql);//成功则返回1；
-
-
-
-//sql语句
-//$result = mysql_query("SELECT * FROM user2",$conn);
-
-	//$result = mysql_query("SELECT * FROM vgene where class='humIGHV'",$conn);
-	//$result = mysql_query("SELECT * FROM vgene where class='".$vclass. "'",$conn);
-
-//构建sql语句	
-if($vclass=="humIGLVAndhumIGKV"){
-	$sql="SELECT * FROM vgene where class in('humIGLV','humIGKV')";
-}else if($vclass=="all"){
-	$sql="SELECT * FROM vgene";
-}else{
-	//$result = mysql_query("SELECT * FROM vgene where class='".$vclass. "'",$conn);
-	$sql="SELECT * FROM vgene where class='".$vclass. "'";
-}
-//执行sql语句
-	$result = mysql_query($sql,$conn);
-
-//分析sql语句	
-if ($myrow = mysql_fetch_array($result)) {
-	$responseT .= "<table class=show border=1>\n";
-	$responseT .= "<tr><th>Id</th><th>GeneName</th><th>class</th><th>sequence</th></tr>\n";
-	
-	$i=0;//hit数
-	$i_totle=0;//查询总数
-	$names="";//hit的v基因名字
-	
-	do {
-		$i_totle++;//查询条目的计算器
-		$template=$myrow["base"];
-		$template2 = preg_replace("/($primer)/is", "<span class=highlight>$1</span>", $template);
-		if($template!=$template2){
-			$names.=$myrow["name"]." ";//命中的V基因名字
-			//命中的V基因详细信息
-			$responseT .='<tr><td>' . $myrow["No"]. '</td><td>'. $myrow["name"].'</td> <td>'. $myrow["class"] .'</td><td>'. $template2.'</td>';
-			$i++;//命中记录的计数器
-		}
-	}while ($myrow = mysql_fetch_array($result));
-	
-	$responseT .= "</table>\n";
-} else {
-	$responseT = "对不起，没有找到记录！"; 
-}
+//获取查询数据库的结果
+$result=getVgeneList( query($vclass) ,$primer);
 
 
 //命中条目总数
-$response.= "<div class=separator>Brief results</div>";
-$response .= "<span class=blue><span class='hit'>Totally hit $i record(s) after searching  $i_totle record(s)</span>. The gene names are:</span><br />";
+$response .= "<div class=separator>Brief results</div>";
+$response .= "<span class=blue><span class='hit'>Totally hit {$result[1]} record(s) after searching  {$result[2]} record(s)</span>. The gene names are:</span><br />";
 
 //命中条目的名字
-$response .="$names";
+$response .=join(', ',$result[3]);
 
-//加上响应的表格
+//加上v基因列表信息
 $response.= "<div class=separator>Detailed results</div>";
-$response.=$responseT."<hr />End of detailed results...";
+$response.=$result[0]."<hr />End of detailed results.";
 
 echo $response;
-//章节1 课时 4 
-// http://study.163.com/course/courseLearn.htm?courseId=706085#/learn/video?lessonId=866409&courseId=706085
 ?>
--- wjl/Vgph/myLib/function.php
+++ wjl/Vgph/myLib/function.php
@@ -1,6 +1,24 @@
 <?php
+/**
+	访问数据库，返回查询后的资源句柄
+*/
+function query($vclass){
+	//构建sql语句	
+	if($vclass=="humIGLVAndhumIGKV"){
+		$sql="SELECT * FROM vgene where class in('humIGLV','humIGKV')";
+	}else if($vclass=="all"){
+		$sql="SELECT * FROM vgene";
+	}else{
+		$sql="SELECT * FROM vgene where class='".$vclass. "'";
+	}
+	//执行sql语句
+	return mysql_query($sql);
+}
 
-//简并引物替换函数
+
+/**
+	简并引物替换函数
+*/
 function replaceATGC($str){
     $str=preg_replace("/R/is", "[AG]", $str);
     $str=preg_replace("/Y/is", "[TC]", $str);
@@ -17,4 +35,49 @@ function replaceATGC($str){
     $str=preg_replace("/N/is", "[ATGC]", $str);
 
     return $str;
+}
+
+
+/**
+根据输入参数，获取vgene列表。
+参数包括：
+	数据库查询的资源句柄： $rows 
+	兼并替换过的引物： $primer
+	该引物是否为反向引物，默认为否：$isReverse=false
+*/
+function getVgeneList($rows,$primer,$isReverse=false){
+	//分析查询结果	
+	$tbl="";//保存响应的表格
+	if ($row = mysql_fetch_array($rows)) {
+		$tbl .= "<table class=show border=1>\n";
+		$tbl .= "<tr><th>Id</th><th>GeneName</th><th>class</th><th>sequence</th></tr>\n";
+		
+		$i=0;//hit数
+		$i_totle=0;//查询总数
+		$aHitNames=array();//hit的v基因名字
+		
+		do {
+			$i_totle++;//查询条目的计算器
+			$template=$row["base"];
+			
+			//引物反向，从5'-3'到3'-5'
+			if($isReverse){
+				$primer=strrev($primer);
+			}
+			
+			//检测是否匹配
+			$template2 = preg_replace("/($primer)/is", "<span class=highlight>$1</span>", $template);
+			if($template!=$template2){
+				$aHitNames[]= $row["name"];//命中的V基因名字
+				//命中的V基因详细信息
+				$tbl .='<tr><td>' . $row["No"]. '</td><td>'. $row["name"].'</td> <td>'. $row["class"] .'</td><td>'. $template2.'</td>';
+				$i++;//命中记录的计数器
+			}
+		}while ($row = mysql_fetch_array($rows));
+		
+		$tbl .= "</table>\n";
+	} else {
+		$tbl = "对不起，没有找到记录！"; 
+	}
+	return array($tbl,$i,$i_totle,$aHitNames);
 }
