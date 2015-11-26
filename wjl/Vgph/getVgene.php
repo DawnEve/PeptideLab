1bb05f645f1b8ca778bdb54abe878c24d71080b9
Jimmy <JimmyMall@live.com>
2015-11-27 07:24:13 +0800

    (1.5)css outside

--- wjl/Vgph/getVgene.php
+++ wjl/Vgph/getVgene.php
@@ -8,15 +8,13 @@ require $path.'myLibGate.php';
 
 //获取class值
 $vclass=get("c",'',true);//禁止直接访问
-//获取引物值
-$primer0=get("q",'CAG GTG CAG CTG CAG GAG TCS G',true);//原始引物序列?
-
+//获取引物值 // 'CAG GTG CAG CTG CAG GAG TCS G'
+$primer0=get("q",'',true);//原始引物序列?这个默认值不起作用
 
 //引物序列确定
 $primer1= preg_replace("/\s/","",$primer0); //删除空格后
 $len=strlen($primer1);//删除空格后的引物长度
 $primer=replaceATGC($primer1);//替换引物中的简并碱基为ATGC
-
 
 //设置变量保存响应值：
 $response = "<div class=separator>Searching parameters</div>";
@@ -27,10 +25,8 @@ $response .= "in the class: $vclass, ";
 $response .= "<b>primer info: </b><div id=primr>
 OriInput:$primer0<br />delSpace:$primer1 length:$len<br />aftSubst:$primer</div>";
 
-
 //获取查询数据库的结果
 $result=getVgeneList( query($vclass) ,$primer);
-
 
 //命中条目总数
 $response .= "<div class=separator>Brief results</div>";
